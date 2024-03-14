e89f7de9fd8e9d43853da5c10cd53920727bcf70
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-03-14 23:37:05 +0200

    12.10

--- todo-app/todo-backend/redis/index.js
+++ todo-app/todo-backend/redis/index.js
@@ -1,27 +1,53 @@
-const redis = require('redis')
-const { promisify } = require('util')
-const { REDIS_URL } = require('../util/config')
+const redis = require('redis');
+const { promisify } = require('util');
+const { REDIS_URL } = require('../util/config');
+const express = require('express');
+const router = express.Router();
 
-let getAsync
-let setAsync
+let getAsync = () => null;
+let setAsync = () => null;
 
-if (!REDIS_URL) {
-  const redisIsDisabled = () => {
-    console.log('No REDIS_URL set, Redis is disabled')
-    return null
-  }
-  getAsync = redisIsDisabled
-  setAsync = redisIsDisabled
-} else {
-  const client = redis.createClient({
-    url: REDIS_URL
-  })
-    
-  getAsync = promisify(client.get).bind(client)
-  setAsync = promisify(client.set).bind(client)    
+if (REDIS_URL) {
+    const client = redis.createClient({
+        url: REDIS_URL
+    });
+
+    client.on('error', err => {
+        console.error('Redis client error:', err);
+    });
+
+    getAsync = promisify(client.get).bind(client);
+    setAsync = promisify(client.set).bind(client);
 }
 
-module.exports = {
-  getAsync,
-  setAsync
-}
+const incrementTodoCounter = async (req, res, next) => {
+    try {
+        const currentCount = await getAsync('todo_counter') || 0;
+        const newCount = parseInt(currentCount) + 1;
+        await setAsync('todo_counter', newCount);
+        next();
+    } catch (error) {
+        console.error('Error incrementing todo counter:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+router.use('/', incrementTodoCounter);
+
+router.get('/', async (req, res) => {
+    try {
+        const todoCount = await getAsync('todo_counter') || 0;
+
+        const statistics = {
+            todo_counter: todoCount
+        };
+        console.log(statistics);
+
+        res.json(statistics); 
+    } catch (error) {
+        console.error('Error fetching statistics:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+module.exports = router;
